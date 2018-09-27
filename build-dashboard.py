2b31f01b74820a9504ae44773d71b58a34e13bfc
Brian Munroe <bmunroe@zappos.com>
2018-09-27 10:28:03 -0700

    refactoring

--- build-dashboard.py
+++ build-dashboard.py
@@ -1,14 +1,19 @@
-import requests, json
+import requests, json, os
 from datetime import datetime, date
+from local_config import *
+
+"""
+pp_username = os.environ['PP_USERNAME']
+pp_password = os.environ['PP_PASSWORD']
+ar_username = os.environ['AR_USERNAME']
+ar_username = os.environ['AR_PASSWORD']
+"""
 
 # Get Plus Portals Assignments
 def get_assignments():
-
-    plus_portals_url="https://www.plusportals.com/LVDS"
-    homework_url='https://www.plusportals.com/ParentStudentDetails/ShowSectionHomeWorkInfo?&isGroup=false'
     
-    r = requests.post(plus_portals_url, data=data, allow_redirects=False)
-    r2 = requests.get(homework_url,cookies=r.cookies)
+    r = requests.post(pp_login_url, data=pp_login_data, allow_redirects=False)
+    r2 = requests.get(pp_homework_url,cookies=r.cookies)
     x = json.loads(r2.text)
 
     d0 = date.today()
@@ -21,19 +26,10 @@ def get_assignments():
 
 # Grab AR Points
 def get_ar_points():
-    ar_login_url = 'https://hosted101.renlearn.com/260290/HomeConnect/Login.aspx'
-    ar_lp_url='https://hosted101.renlearn.com/260290/HC/Reading/ARReadingPractice.aspx'
 
-    
-    data['__VIEWSTATE']='/wEPDwUKLTkyMDU5NzIwNA9kFgICAQ9kFgICAQ9kFgICAQ8PFgIeB1Zpc2libGVnZGRkgxa8DgsFgj8Gef7E0nyqMNG4oTsXq+QaDd465kj//nc='
-    data['__EVENTVALIDATION']='/wEdAAV911NEnmlz5dto7LmADeF7lU3ax5wVG3nfYXTHNadz5WXM+3WtjHCIr5YiZBbD32AtSTitspZrFBypMFcQCUvzFPOBqWRHCNGau92f78GaLvwt+OQBZ5xsxGmQ8lCRaTWQbqB0gNM5sq0IYKRaqEMn'
-    data['mBox_Login$mButton_LogIn']='Log In'
-    # data['__EVENTTARGET']=''
-    # data['__EVENTARGUMENT']=''
-
-    r1 = requests.post(ar_login_url,data=data,allow_redirects=False)
+    r1 = requests.post(ar_login_url,data=ar_login_data,allow_redirects=False)
     r2 = requests.post(ar_lp_url,cookies=r1.cookies)
     print(r2.text)
 
-# get_assignments()
-get_ar_points()
+get_assignments()
+# get_ar_points()
