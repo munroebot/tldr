2d05d204e479c5f1350ca3a5d6c7646e6ec67754
Brian Munroe <bmunroe@zappos.com>
2018-10-05 08:52:03 -0700

    updating the build script for lambda

--- build-lambda.sh
+++ build-lambda.sh
@@ -7,9 +7,6 @@ BUILD_DIR=${PWD};
 TLDR_VIRTUALENV=".";
 
 echo "Rebuilding Deployment Package...";
-
-rm -f tldr.zip
-
 cd ${TLDR_VIRTUALENV}/lib/python3.7/site-packages
 zip -r9 ${BUILD_DIR}/tldr.zip *
 cd ${BUILD_DIR};
@@ -19,14 +16,13 @@ zip -g tldr.zip TLDR.py;
 zip -g tldr.zip local_config.py;
 zip -g tldr.zip email_templates.py;
 
-# echo "Pushing to AWS...";
+echo "Pushing to AWS...";
+for i in ${functions}; do
+    aws lambda update-function-code \
+    --region us-east-1 \
+    --function-name ${i} \
+    --zip-file fileb://./tldr.zip
+done
 
-# aws lambda update-function-code \
-# --region us-east-1 \
-# --function-name TLDR \
-# --zip-file fileb://./tldr.zip \
-# --role ${AWS_ROLE} \
-# --handler TLDR.lambda_handler \
-# --runtime python3.6 \
-# --timeout 30 \
-# --memory-size 1024;
+echo "Cleaning up artifact...";
+rm -f tldr.zip
